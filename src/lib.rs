149eda5ca5ad74d0992a4d543bce61d302b32c50
Softgod <softgod4@protonmail.com>
2024-08-04 15:51:21 +0300

    add event listener and css

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use web_sys::{Document, Element, HtmlElement};
+use web_sys::{js_sys::JsString, Document, Element, HtmlElement};
 
 #[wasm_bindgen]
 extern "C" {
@@ -24,9 +24,28 @@ fn dcmnt() -> Document {
 pub fn greet() -> Result<(), JsValue> {
     let render: Document = dcmnt();
     let body: HtmlElement = render.body().expect("document should have a body");
-    let container = body.query_selector(".container").unwrap().expect("");
+    let container: Element = body.query_selector(".container").unwrap().expect("");
+
+    // event-listener
+    let closure: Closure<dyn FnMut(HtmlElement)> =
+        Closure::<dyn FnMut(_)>::new(move |event: web_sys::HtmlElement| {
+            let input = dcmnt()
+                .get_element_by_id("input-task")
+                .unwrap()
+                .dyn_into::<HtmlElement>();
+            console_log!("{:?}", input);
+        });
+
+    let button = render
+        .get_element_by_id("add-task")
+        .unwrap()
+        .add_event_listener_with_callback("click", closure.as_ref().unchecked_ref());
+
+    closure.forget();
+
+    // testing
     let i: u32 = 1;
-    for _n in i..100 {
+    for _n in i..5 {
         let val: Element = render.create_element("p")?;
         val.set_text_content(Some("abc"));
         container.append_child(&val)?;
