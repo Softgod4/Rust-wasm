9a5d27464fc6f31d5f434268b696d982849e04d4
Softgod <softgod4@protonmail.com>
2024-08-06 19:16:55 +0300

    program ready

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
 mod task_inner;
 
+use std::fmt::Error;
+
 use once_cell::sync::Lazy;
 use regex::Regex;
 use wasm_bindgen::prelude::*;
@@ -24,7 +26,9 @@ fn dcmnt() -> Document {
 }
 
 fn value_test(str: String) -> bool {
-	static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"[^a-zA-Z\s]").unwrap());
+	static RE: Lazy<Regex> = Lazy::new(||
+		Regex::new(r"(^$|[^a-zA-Z\s])").unwrap()
+	);
 	RE.is_match(&str)
 }
 
@@ -33,6 +37,7 @@ fn value_test(str: String) -> bool {
 pub fn create_task(task_name: String) -> Result<(), JsValue> {
 	let render: Document = dcmnt();
 	let body: HtmlElement = render.body().expect("document should have a body");
+	let _ = delete_not_valide();
 	let tasks_list: web_sys::Element = body
 		.query_selector(".tasks-list")
 		.unwrap()
@@ -46,7 +51,20 @@ pub fn create_task(task_name: String) -> Result<(), JsValue> {
 }
 
 #[wasm_bindgen]
-pub fn not_valide_input() -> Result<(), JsValue> {
+pub fn delete_task() -> Result<(), JsError> {
+	let item = dcmnt()
+		.query_selector(".tasks-list__item")
+		.map_err(|_| JsError::new("Query selector failed"))?
+		.ok_or_else(|| JsError::new("No tasks found"))?;
+
+	item.remove();
+
+	Ok(())
+}
+
+// not valide input
+#[wasm_bindgen]
+pub fn not_valide() -> Result<(), JsValue> {
 	let render: Document = dcmnt();
 	let body: HtmlElement = render.body().expect("document should have a body");
 	let not_valide_message: web_sys::Element = body
@@ -70,44 +88,76 @@ pub fn not_valide_input() -> Result<(), JsValue> {
 	Ok(())
 }
 
+// for delete not valide input
+#[wasm_bindgen]
+pub fn delete_not_valide() -> Result<(), JsValue> {
+	let render: Document = dcmnt();
+	let body: HtmlElement = render.body().expect("document should have a body");
+	match body.query_selector(".not-valid") {
+		Ok(Some(_)) => {
+			let valid = body.query_selector(".not-valid").unwrap().expect("");
+			valid.remove();
+		}
+		Ok(None) => {
+			console_log!("not exists");
+		}
+		Err(e) => {
+			console_log!("{:?}", e);
+		}
+	}
+	Ok(())
+}
+
+// main function
 #[wasm_bindgen]
 pub fn greet() -> Result<(), JsValue> {
 	let render: Document = dcmnt();
 
-	// let body: HtmlElement = render.body().expect("document should have a body");
-	// let container: Element = body
-	// 	.query_selector(".container")
-	// 	.unwrap()
-	// 	.expect("");
+	// event-listener
+	let add_closure: Closure<dyn FnMut(HtmlElement)> = Closure::<
+		dyn FnMut(_)
+	>::new(move |_: web_sys::HtmlElement| {
+		let input_value: String = dcmnt()
+			.get_element_by_id("input-task")
+			.unwrap()
+			.dyn_into::<HtmlInputElement>()
+			.unwrap()
+			.value();
+
+		if !value_test(input_value.clone()) {
+			console_log!("{}", input_value);
+			let _ = create_task(input_value);
+		} else {
+			let _ = not_valide();
+		}
+	});
 
 	// event-listener
-	let closure: Closure<dyn FnMut(HtmlElement)> = Closure::<dyn FnMut(_)>::new(
-		move |_: web_sys::HtmlElement| {
-			let input_value: String = dcmnt()
-				.get_element_by_id("input-task")
-				.unwrap()
-				.dyn_into::<HtmlInputElement>()
-				.unwrap()
-				.value();
+	let delete_closure: Closure<dyn FnMut(HtmlElement)> = Closure::<
+		dyn FnMut(_)
+	>::new(move |_: web_sys::HtmlElement| {
+		let _ = delete_task();
+	});
 
-			if !value_test(input_value.clone()) {
-				console_log!("{}", input_value);
-				let _ = create_task(input_value);
-			} else {
-				let _ = not_valide_input();
-			}
-		}
-	);
-
+	// buttons
 	let _button: Result<(), JsValue> = render
 		.get_element_by_id("add-task")
 		.unwrap()
 		.add_event_listener_with_callback(
 			"click",
-			closure.as_ref().unchecked_ref()
+			add_closure.as_ref().unchecked_ref()
 		);
 
-	closure.forget();
+	let _delete_btn: Result<(), JsValue> = render
+		.get_element_by_id("remove-all")
+		.unwrap()
+		.add_event_listener_with_callback(
+			"click",
+			delete_closure.as_ref().unchecked_ref()
+		);
+
+	add_closure.forget();
+	delete_closure.forget();
 
 	// testing
 	// let i: u32 = 1;
